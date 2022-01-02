5dda1a471c35ac6f98b83e6d9e70f71ec4c93336
Evgenia Kurineva <kurineva85@mail.ru>
2022-01-02 13:02:38 +0300

    feat: add no winners to Winners page

--- src/pages/winnersView.ts
+++ src/pages/winnersView.ts
@@ -5,6 +5,7 @@ const renderWinnersView = () => `
 <div class='winners-view hide'>
   <h2>${VIEW_NAME.winners} (${store.allWinnersCount})</h2>
   <h3>Page#${store.winnersCurrentPageNum}</h3>
+  <p>${store.allWinnersCount === '0' ? 'There are no winners yet' : ''}</p>
 </div>`;
 
 export default renderWinnersView;
