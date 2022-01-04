d8a8d77e47e084fb237c90cbb472052d794721c3
Evgenia Kurineva <kurineva85@mail.ru>
2022-01-04 12:02:37 +0300

    refactor: use common handler for all clicks

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import renderApp from './app';
 import { VIEW_NAME } from './constants';
 
 import './index.scss';
-import store from './store';
 import createCar from './utils/createCar';
+import linkTo from './utils/linkTo';
 import selectCar from './utils/selectCar';
 
 const root = document.querySelector('#root');
 root.innerHTML = renderApp();
 
-const garageViewEl = document.querySelector('.garage-view');
-const winnersViewEl = document.querySelector('.winners-view');
+const garageViewEl = document.querySelector('.garage-view') as HTMLElement;
+const winnersViewEl = document.querySelector('.winners-view') as HTMLElement;
 
 const createCarNameInput = document.querySelector('#create-car-name') as HTMLInputElement;
 const createCarColorInput = document.querySelector('#create-car-color') as HTMLInputElement;
@@ -19,24 +19,14 @@ const updateCarNameInput = document.querySelector('#update-car-name') as HTMLInp
 const updateCarColorInput = document.querySelector('#update-car-color') as HTMLInputElement;
 const updateSubmit = document.querySelector('#submit-update-car') as HTMLInputElement;
 
-const toGarageHandler = () => {
-    store.currentView = VIEW_NAME.garage;
-    winnersViewEl.classList.add('hide');
-    garageViewEl.classList.remove('hide');
-};
-document.querySelector('#link-to-garage').addEventListener('click', toGarageHandler);
-
-const toWinnersHandler = () => {
-    store.currentView = VIEW_NAME.garage;
-    garageViewEl.classList.add('hide');
-    winnersViewEl.classList.remove('hide');
-};
-document.querySelector('#link-to-winners').addEventListener('click', toWinnersHandler);
-
 const handlerOnClick = (event: MouseEvent) => {
     const { id } = event.target as HTMLElement;
     console.log(2, id);
-    if (id === 'submit-create-car') {
+    if (id === 'link-to-winners') {
+        linkTo(garageViewEl, winnersViewEl, VIEW_NAME.winners);
+    } else if (id === 'link-to-garage') {
+        linkTo(winnersViewEl, garageViewEl, VIEW_NAME.garage);
+    } else if (id === 'submit-create-car') {
         createCar(event, createCarNameInput, createCarColorInput);
     } else if (id.includes('select-car-')) {
         selectCar(event, id, updateCarNameInput, updateCarColorInput, updateSubmit);
